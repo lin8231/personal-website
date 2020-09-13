6af17560b4ff3ac606c7998b20b1f7ba998aa187
Jiaru <lin8231@outlook.com>
2020-09-13 23:20:48 +1200

    First commit

---
+++ src/MainPage/IconList/IconList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import githubIcon from '../assets/github.svg';
+import classes from './IconList.module.css';
+
+const IconList = props =>{
+
+    return (
+        <div className={classes.Container}>
+            hi
+            {/* <img src={githubIcon} alt=""/> */}
+            hi
+        </div>
+    )
+}
+
+export default IconList;
