d116584a2866a0e9da10dd06fb649132850544c7
Jiaru <lin8231@outlook.com>
2020-09-14 02:43:08 +1200

    Getting rid of warnings

--- src/MainPage/IconList/IconList.js
+++ src/MainPage/IconList/IconList.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import classes from './IconList.module.css';
 
-import { ReactComponent as GithubIcon } from '../../assets/github.svg';
-import { ReactComponent as LinkedinIcon } from '../../assets/linkedin.svg';
-import { ReactComponent as ResumeIcon } from '../../assets/resume.svg';
-import { ReactComponent as EmailIcon } from '../../assets/email.svg';
-
 import githubIcon from '../../assets/github.svg';
 import linkedinIcon from '../../assets/linkedin.svg';
 import resumeIcon from '../../assets/resume.svg';
@@ -28,22 +23,22 @@ const IconList = props => {
 
             <a href="https://github.com/lin8231"
                 className={classes.IconContainer}
-                target="_blank">
+                target="_blank" rel="noopener noreferrer">
                 <img src={githubIcon} alt="" className={classes.Icon} />
             </a>
             <a href="https://www.linkedin.com/in/mercury-jiaru-lin-a79a5b1a4/"
                 className={classes.IconContainer}
-                target="_blank">
+                target="_blank" rel="noopener noreferrer">
                 <img src={linkedinIcon} alt="" className={classes.Icon} />
             </a>
             <a href={cv} download
                 className={classes.IconContainer}
-                target="_blank">
+                target="_blank" rel="noopener noreferrer">
                 <img src={resumeIcon} alt="" className={classes.Icon} />
             </a>
             <a href="mailto:lin8231@outlook.com"
                 className={classes.IconContainer}
-                target="_blank">
+                target="_blank" rel="noopener noreferrer">
                 <img src={emailIcon} alt="" className={classes.Icon} />
             </a>
 
